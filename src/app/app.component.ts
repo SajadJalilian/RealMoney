b61bbb0e87f3262b1238e10d75593922531b1448
Sajad Jalilian <sajadjdev@gmail.com>
2025-03-14 14:14:34 +0330

    Add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
